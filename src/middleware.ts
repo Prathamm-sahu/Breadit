215fe64a73d0a9c8cf8c8b87181f038f72dae85a
Pratham Sahu <prathamsahu051@gmail.com>
2024-02-24 14:32:13 +0530

    just checking

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
 
   if (!token) {
+    console.log("Pratham", token)
     return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
   }
 }
