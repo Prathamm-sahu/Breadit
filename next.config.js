1269d96eecdb31c4d6b652cb0c725aba71dcf786
Pratham Sahu <prathamsahu051@gmail.com>
2024-03-11 16:35:29 +0530

    update nextconfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
   },
+  i18n: {
+    localeDetection: false,
+  },
 }
 
 module.exports = nextConfig
